8905ececfd5ba97866532d4feb0f57f01c3fa115
Swarna Shikhar Das <swarnashikhar2002@gmail.com>
2024-06-26 10:47:20 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "dynamic-woodpecker-199.convex.cloud"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
